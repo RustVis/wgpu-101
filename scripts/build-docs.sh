dae99a0ccb6397a3d76dd6688fffa4a22d551c2a
Xu Shaohua <shaohua@biofan.org>
2023-02-17 00:20:52 +0800

    Add scripts/

---
+++ scripts/build-docs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+# Use of this source is governed by General Public License that can be found
+# in the LICENSE file.
+
+set -xe
+
+which mdbook || cargo install mdbook
+mdbook build
