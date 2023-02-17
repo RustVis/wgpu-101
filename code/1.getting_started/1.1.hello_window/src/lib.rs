6222d56bdb3b4b279f866f2fe2881214548cb466
Xu Shaohua <shaohua@biofan.org>
2023-02-17 09:03:36 +0800

    Handle errors with anyhow

--- code/1.getting_started/1.1.hello_window/src/lib.rs
+++ code/1.getting_started/1.1.hello_window/src/lib.rs
@@ -2,29 +2,35 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
-use winit::{
-    event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::{Window, WindowBuilder},
-};
+use winit::dpi::PhysicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{Window, WindowBuilder};
 
-fn event_loop_handler(event: Event, control_flow: ControlFlow, window: Window) {
+fn event_loop_handler<T>(event: &Event<T>, control_flow: &mut ControlFlow, window: &Window) {
     match event {
         Event::WindowEvent {
-            ref window_event,
+            ref event,
             window_id,
-        } if window_id == window.id() => match window_event {
-            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-        },
+            ..
+        } if *window_id == window.id() && event == &WindowEvent::CloseRequested => {
+            *control_flow = ControlFlow::Exit;
+        }
         _ => {}
     }
 }
 
-pub fn run() {
+/// # Errors
+/// Returns error if failed to create main window.
+pub fn run() -> anyhow::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
+    log::info!("Creating a new with with size 800x600");
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let window = WindowBuilder::new()
+        .with_title("Hello Window")
+        .with_inner_size(PhysicalSize::new(800, 600))
+        .build(&event_loop)?;
 
-    event_loop.run(move |event, _, control_flow| event_loop_handler(event, control_flow, window));
+    event_loop.run(move |event, _, control_flow| event_loop_handler(&event, control_flow, &window));
 }
