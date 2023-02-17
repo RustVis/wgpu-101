3d532c4e3f70517b96592e0fd3984526f2963245
Xu Shaohua <shaohua@biofan.org>
2023-02-17 14:01:30 +0800

    Add index-buffer

---
+++ code/1.getting_started/ch02.04.index_buffer/src/error.rs
@@ -0,0 +1,42 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+use std::io;
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::JsValue;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("io error")]
+    Io(#[from] io::Error),
+
+    #[error("wgpu device error")]
+    WgpuDevice(#[from] wgpu::RequestDeviceError),
+
+    #[error("wgpu surface error")]
+    WpugSurface(#[from] wgpu::CreateSurfaceError),
+
+    #[error("window error")]
+    Winit(#[from] winit::error::OsError),
+
+    #[error("web error")]
+    Web(String),
+
+    #[error("other error")]
+    Others(String),
+}
+
+#[cfg(target_arch = "wasm32")]
+impl Into<JsValue> for Error {
+    fn into(self) -> JsValue {
+        JsValue::from_str(&format!("{self:?}"))
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+impl From<JsValue> for Error {
+    fn from(val: JsValue) -> Self {
+        Self::Web(val.as_string().unwrap_or_default())
+    }
+}
