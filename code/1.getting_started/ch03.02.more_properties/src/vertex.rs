5d89d99486765e1f86ee25dfc97eab23973dd9ce
Xu Shaohua <shaohua@biofan.org>
2023-02-18 09:05:42 +0800

    Fix vertex position error

--- code/1.getting_started/ch03.02.more_properties/src/vertex.rs
+++ code/1.getting_started/ch03.02.more_properties/src/vertex.rs
@@ -25,17 +25,14 @@ impl Vertex {
 }
 
 pub const VERTICES: &[Vertex] = &[
-    // bottom right
-    Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
-    },
-    // bottom left
     Vertex {
         position: [-0.5, -0.5, 0.0],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, 0.0],
         color: [0.0, 1.0, 0.0],
     },
-    // top
     Vertex {
         position: [0.0, 0.5, 0.0],
         color: [0.0, 0.0, 1.0],
