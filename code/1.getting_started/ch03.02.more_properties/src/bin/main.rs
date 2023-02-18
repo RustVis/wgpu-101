61bfc68113398745150ee02783b2d47dac199742
Xu Shaohua <shaohua@biofan.org>
2023-02-18 08:51:02 +0800

    Add more properties to vertex

---
+++ code/1.getting_started/ch03.02.more_properties/src/bin/main.rs
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+use ch03_02_more_properties::{run, Error};
+
+fn main() -> Result<(), Error> {
+    pollster::block_on(run())
+}
