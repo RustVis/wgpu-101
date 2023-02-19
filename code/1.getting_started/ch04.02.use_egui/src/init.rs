ff2a9b130a529e7f55445ea9beeb5e0f3bd2a7b2
Xu Shaohua <shaohua@biofan.org>
2023-02-19 10:59:30 +0800

    Add use-egui

---
+++ code/1.getting_started/ch04.02.use_egui/src/init.rs
@@ -0,0 +1,85 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+cfg_if::cfg_if! {
+    if #[cfg(target_arch = "wasm32")] {
+        use wasm_bindgen::prelude::wasm_bindgen;
+        use winit::platform::web::WindowExtWebSys;
+    }
+}
+use winit::dpi::PhysicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{Window, WindowBuilder};
+
+use crate::{Error, State};
+
+fn event_loop_handler<T>(event: &Event<T>, control_flow: &mut ControlFlow, state: &mut State) {
+    match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+            ..
+        } if *window_id == state.window().id() => {
+            if !state.input(event) {
+                match event {
+                    WindowEvent::Resized(physical_size) => state.resize(*physical_size),
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        state.resize(**new_inner_size);
+                    }
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    _ => {}
+                }
+            }
+        }
+        Event::RedrawRequested(window_id) if *window_id == state.window().id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size()),
+                Err(wgpu::SurfaceError::OutOfMemory) => {
+                    log::error!("System out of memory!");
+                    *control_flow = ControlFlow::Exit;
+                }
+                Err(err) => log::error!("{err:?}"),
+            }
+        }
+
+        Event::MainEventsCleared => state.window().request_redraw(),
+        _ => {}
+    }
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub async fn run() -> Result<(), Error> {
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            wasm_logger::init(wasm_logger::Config::default());
+        } else {
+            env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+        }
+    }
+
+    log::info!("Creating a new with with size 800x600");
+    let event_loop = EventLoop::new();
+    let window: Window = WindowBuilder::new()
+        .with_title("Use egui")
+        .with_inner_size(PhysicalSize::new(800, 600))
+        .build(&event_loop)?;
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        let element = web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| doc.get_element_by_id("wasm-container"))
+            .ok_or_else(|| Error::Web("failed to find dom element '#wasm-container'".to_owned()))?;
+        let canvas = web_sys::Element::from(window.canvas());
+        element.append_child(&canvas)?;
+    }
+
+    let mut state = State::new(window).await?;
+
+    event_loop
+        .run(move |event, _, control_flow| event_loop_handler(&event, control_flow, &mut state));
+}
---
+++ code/1.getting_started/ch04.02.use_egui/src/state.rs
@@ -0,0 +1,314 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+use std::time;
+use cgmath::Vector3;
+use wgpu::util::DeviceExt;
+use winit::dpi::PhysicalSize;
+use winit::event::WindowEvent;
+use winit::window::Window;
+
+use crate::vertex::{Vertex, VERTICES};
+use crate::Error;
+
+const ANIMATION_SPEED: f32 = 1.0;
+
+#[derive(Debug)]
+pub struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: PhysicalSize<u32>,
+    window: Window,
+
+    vertex_color: Vector3<f32>,
+    uniform_buffer: wgpu::Buffer,
+    uniform_bind_group: wgpu::BindGroup,
+
+    render_pipeline: wgpu::RenderPipeline,
+
+    vertex_buffer: wgpu::Buffer,
+    num_vertices: u32,
+
+    start_time: time::Instant,
+}
+
+impl State {
+    async fn create_surface(
+        window: &Window,
+    ) -> Result<
+        (
+            wgpu::Surface,
+            wgpu::Device,
+            wgpu::Queue,
+            wgpu::SurfaceConfiguration,
+            PhysicalSize<u32>,
+        ),
+        Error,
+    > {
+        let size = window.inner_size();
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            ..Default::default()
+        });
+
+        let surface = unsafe { instance.create_surface(&window) }?;
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .ok_or_else(|| {
+                Error::Others("Failed to get an approprivate wgpu adapter".to_owned())
+            })?;
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: if cfg!(target_arch = "wasm32") {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    },
+                    label: None,
+                },
+                None,
+            )
+            .await?;
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .copied()
+            .filter(|f| f.describe().srgb)
+            .next()
+            .unwrap_or(surface_caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+        };
+        surface.configure(&device, &config);
+
+        Ok((surface, device, queue, config, size))
+    }
+
+    fn create_uniform_buffer(
+        device: &wgpu::Device,
+        color: &[f32; 3],
+    ) -> (wgpu::Buffer, wgpu::BindGroupLayout, wgpu::BindGroup) {
+        let uniform_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Uniform Buffer"),
+            contents: bytemuck::cast_slice(color),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+        let uniform_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: Some("Uniform Bind Group Layout"),
+                entries: &[wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }],
+            });
+        let uniform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Uniform Bind Group"),
+            layout: &uniform_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: uniform_buffer.as_entire_binding(),
+            }],
+        });
+
+        (
+            uniform_buffer,
+            uniform_bind_group_layout,
+            uniform_bind_group,
+        )
+    }
+
+    fn create_render_pipeline(
+        device: &wgpu::Device,
+        config: &wgpu::SurfaceConfiguration,
+        uniform_bind_group_layout: &wgpu::BindGroupLayout,
+    ) -> wgpu::RenderPipeline {
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("../res/shaders/triangle.wgsl").into()),
+        });
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[uniform_bind_group_layout],
+                push_constant_ranges: &[],
+            });
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[Vertex::desc()],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
+
+        render_pipeline
+    }
+
+    pub async fn new(window: Window) -> Result<Self, Error> {
+        let (surface, device, queue, config, size) = Self::create_surface(&window).await?;
+
+        let vertex_color = Vector3::new(0.3, 0.4, 0.5);
+        let vertex_color_ref = vertex_color.as_ref();
+
+        let (uniform_buffer, uniform_bind_group_layout, uniform_bind_group) =
+            Self::create_uniform_buffer(&device, vertex_color_ref);
+
+        let render_pipeline =
+            Self::create_render_pipeline(&device, &config, &uniform_bind_group_layout);
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(VERTICES),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+        let num_vertices = VERTICES.len() as u32;
+
+        Ok(Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+
+            vertex_color,
+            uniform_buffer,
+            uniform_bind_group,
+
+            render_pipeline,
+
+            vertex_buffer,
+            num_vertices,
+
+            start_time: time::Instant::now(),
+        })
+    }
+
+    pub fn size(&self) -> PhysicalSize<u32> {
+        self.size
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    pub fn resize(&mut self, new_size: PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    pub fn input(&mut self, _event: &WindowEvent) -> bool {
+        false
+    }
+
+    pub fn update(&mut self) {
+        let dt = self.start_time.elapsed();
+        let dt = ANIMATION_SPEED * dt.as_secs_f32();
+        self.vertex_color.x = dt.sin();
+        self.vertex_color.y = dt.cos();
+        // log::info!("vertex color: {}:{}", self.vertex_color.x, self.vertex_color.y);
+        let vertex_color_ref: &[f32; 3] = self.vertex_color.as_ref();
+
+        self
+            .queue
+            .write_buffer(&self.uniform_buffer, 0, bytemuck::cast_slice(vertex_color_ref));
+    }
+
+    pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.2,
+                            g: 0.3,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_bind_group(0, &self.uniform_bind_group, &[]);
+            render_pass.draw(0..self.num_vertices, 0..1);
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
