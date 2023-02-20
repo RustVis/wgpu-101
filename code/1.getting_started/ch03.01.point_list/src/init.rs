1c530cb1088f5566e15a7944fd074a2bfe9960f7
Xu Shaohua <shaohua@biofan.org>
2023-02-20 22:14:49 +0800

    Update title of windows

--- code/1.getting_started/ch03.01.point_list/src/init.rs
+++ code/1.getting_started/ch03.01.point_list/src/init.rs
@@ -64,7 +64,7 @@ pub async fn run() -> Result<(), Error> {
     log::info!("Creating a new with with size 800x600");
     let event_loop = EventLoop::new();
     let window: Window = WindowBuilder::new()
-        .with_title("Hello Triangle")
+        .with_title("Hello Point List")
         .with_inner_size(PhysicalSize::new(800, 600))
         .build(&event_loop)?;
 
