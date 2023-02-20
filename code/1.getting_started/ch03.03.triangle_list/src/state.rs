d3b86f84cd2f88970cc23b1acc2b872d989c3db8
Xu Shaohua <shaohua@biofan.org>
2023-02-20 22:13:55 +0800

    Tuning vertex positions

--- code/1.getting_started/ch03.03.triangle_list/src/state.rs
+++ code/1.getting_started/ch03.03.triangle_list/src/state.rs
@@ -125,7 +125,8 @@ impl State {
                 })],
             }),
             primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList,
+                //topology: wgpu::PrimitiveTopology::TriangleList,
+                topology: wgpu::PrimitiveTopology::TriangleStrip,
                 strip_index_format: None,
                 front_face: wgpu::FrontFace::Ccw,
                 cull_mode: Some(wgpu::Face::Back),
