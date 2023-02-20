7c3d9400edf3314c4b99c5b390554b23c885fb92
Xu Shaohua <shaohua@biofan.org>
2023-02-20 21:16:11 +0800

    Add primitives

--- code/1.getting_started/ch03.03.colorful_rect/src/bin/main.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
-// Use of this source is governed by General Public License that can be found
-// in the LICENSE file.
-
-use ch03_03_colorful_rect::{run, Error};
-
-fn main() -> Result<(), Error> {
-    pollster::block_on(run())
-}
