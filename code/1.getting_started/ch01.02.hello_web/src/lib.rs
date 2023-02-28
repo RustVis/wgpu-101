16ffbfc1f0d3efba7142827ccdfc09d728635c1e
Xu Shaohua <shaohua@biofan.org>
2023-02-28 14:38:40 +0800

    Update title of pages

--- code/1.getting_started/ch01.02.hello_web/src/lib.rs
+++ code/1.getting_started/ch01.02.hello_web/src/lib.rs
@@ -43,7 +43,7 @@ pub async fn run() -> Result<(), Error> {
     log::info!("Creating a new with with size 800x600");
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
-        .with_title("Hello Web")
+        .with_title("Hello WebGPU")
         .with_inner_size(PhysicalSize::new(800, 600))
         .build(&event_loop)?;
 
