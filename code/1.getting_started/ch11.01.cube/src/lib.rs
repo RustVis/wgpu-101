c0399ac3f8fe2283372633927f41a1e83cb8033a
Xu Shaohua <shaohua@biofan.org>
2023-03-09 10:31:30 +0800

    Add cube geometry

---
+++ code/1.getting_started/ch11.01.cube/src/lib.rs
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+mod camera;
+mod error;
+mod frames;
+mod init;
+mod state;
+mod texture;
+mod uniforms;
+mod vertex;
+
+pub use self::error::Error;
+pub use self::init::run;
+pub use self::state::State;
