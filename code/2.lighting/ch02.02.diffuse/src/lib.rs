0fc682b4b4295ef113a1bed49e56d587b1537fe8
Xu Shaohua <shaohua@biofan.org>
2023-03-14 10:50:54 +0800

    lighting: Add diffuse

---
+++ code/2.lighting/ch02.02.diffuse/src/lib.rs
@@ -0,0 +1,21 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+mod box_scene;
+mod camera;
+mod error;
+mod frames;
+mod geometry;
+mod init;
+mod light_scene;
+mod scenes;
+mod state;
+mod texture;
+mod transforms;
+mod uniforms;
+mod vertex;
+
+pub use self::error::Error;
+pub use self::init::run;
+pub use self::state::State;
